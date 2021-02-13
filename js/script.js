deadf45200fcb953040f0f33e2035ae717d517e8
Lechosław <lechoslaw.sulek@gmail.com>
2021-02-13 11:09:19 +0100

    Add moves

--- js/script.js
+++ js/script.js
@@ -1,7 +1,90 @@
-let computerMove = `kamień`;
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 
-let playerMove = `papier`;
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+if(randomNumber == 2){
+    computerMove = 'papier';
+}
+if(randomNumber == 3){
+computerMove = 'nożyce';
+}
+
+
+
+/* śmieci
+if(1 > 2){
+	printMessage('Niesamowite! Jeden jest większe niż dwa!!!');
+} else if (1 == 2) {
+	printMessage('Dziwne – jeden jest równe dwa?!');
+} else {
+	printMessage('Wszystko po staremu, jeden jest mniejsze niż dwa.');
+}
+
+
+printMessage('Mój ruch to: ' + computerMove);
+*/
+/*
+KODILLAKOPIA
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+*/
+
+/*
+MOJE
+let randomFraction = Math.random();
+
+// wywołanie funkcji z jednym argumentem
+printMessage('Wylosowany ułamek to: ' + randomFraction);
+
+let calculation = randomFraction * 9 + 11;
+
+printMessage('Ułamek pomnożony przez 9 i powiększony o 11: ' + calculation);
+
+let roundNumber = Math.floor(calculation);
+
+printMessage('Liczba po zaokrągleniu w dół to: ' + roundNumber);
+*/
+
+
+/* DODATKI DO WYJAŚNIENIA
+let randomNumber = Math.floor(Math.random() * 9 + 11);
+printMessage('Wylosowana liczba z zakresu od 11 do 19 to: ' + roundNumber);
+/*
+
+
+
+
+// let randomNumber = Math.flooralert(rand(11, 19));
+// printMessage('Wylosowana liczba z zakresu od 11 do 19 to: ' + roundNumber);
+
+// let randomNumber = alert(rand(11, 19));
+
+// Math.floor(Math.random() * 10) + 1;  // returns a random integer from 1 to 10
+*/
