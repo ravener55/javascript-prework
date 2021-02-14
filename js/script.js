b753d2a05eceeff6739dc6d88233f1c11a3ec813
Lechosław <lechoslaw.sulek@gmail.com>
2021-02-14 11:33:56 +0100

    Add all draws and results

--- js/script.js
+++ js/script.js
@@ -1,19 +1,61 @@
 let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-
 console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = 'nieznany ruch';
-
 if(randomNumber == 1){
-  computerMove = 'kamień';
-}
-if(randomNumber == 2){
+    computerMove = 'kamień';
+} else if(randomNumber == 2){
     computerMove = 'papier';
+} else if(randomNumber == 3){
+    computerMove = 'nożyce';
 }
-if(randomNumber == 3){
-computerMove = 'nożyce';
+printMessage('Ruch komputera: ' + computerMove);
+
+let playerMove = 'nieznany ruch';
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Mój ruch: ' + playerInput);
+if(playerInput == 1){
+    playerMove = 'kamień';
+}   else if(playerInput == 2){
+    playerMove = 'papier';
+}    else if(playerInput == 3){
+    playerMove = 'nożyce';
 }
+printMessage('Ruch gracza: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis!');
+} else if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+} else if( computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Komputer wygrywa!');
+} else if( computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Komputer wygrywa!');
+} else if( computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis!');
+} else if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+}if( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+} else if( computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Komputer wygrywa!');
+} else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Remis!');
+}
+
+/*
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+*/
 
 
 
